2c88539c1df0f6b79327bd1fc276c4ed9d17e584
apetit <alainpetit21@hotmail.com>
2019-10-09 16:41:04 -0400

    Added logs

--- Src/Controller/AppBitCoinNodeAutomationForCrashedRam.py
+++ Src/Controller/AppBitCoinNodeAutomationForCrashedRam.py
@@ -28,18 +28,32 @@ class AppBitCoinNodeAutomationForCrashedRam(App):
 
         try:
             while True:
+                print("\nRestarting Time now" + str(pk_time.time()))
+
+                print("\nPerforming moveTo(150, 150, duration=2)")
                 pk_py_auto_gui.moveTo(150, 150, duration=2)
+
+                print("\nPerforming click(150, 150");
                 pk_py_auto_gui.click(150, 150)
+
+                print("\nPerforming typewrite('bitcoin-qt -datadir=/home/pi/SSD_1Tb/bitcoinData')");
                 pk_py_auto_gui.typewrite('bitcoin-qt -datadir=/home/pi/SSD_1Tb/bitcoinData')
+
+                print("\nPerforming press('enter')");
                 pk_py_auto_gui.press('enter')
 
+                print("\nPerforming sleep(3 * 60)");
                 # pk_time.sleep(2)
                 pk_time.sleep(3 * 60)
                 # pk_time.sleep(2*60*60)
 
+                print("\nPerforming click(150, 150)");
                 pk_py_auto_gui.click(150, 150)
+
+                print("\nPerforming hotkey('ctrl', 'c')");
                 pk_py_auto_gui.hotkey('ctrl', 'c')
 
+                print("\nPerforming sleep(10 * 60)");
                 pk_time.sleep(10 * 60)
 
         except KeyboardInterrupt:
